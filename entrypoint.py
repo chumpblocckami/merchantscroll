d728f3342bb5291ec345fe4b91f15fd7259cbd7c
chumpblocckami <mazzolauni@gmail.com>
2025-05-15 11:10:00 +0200

    feat: updates

--- entrypoint.py
+++ entrypoint.py
@@ -8,19 +8,17 @@ from src.crawler import crawl_decks, crawl_tournaments
 
 def start_crawler():
     with open(TOURNAMENT_FILE_PATH, "r") as f:
-        crawled_tournaments = f.readlines()
+        crawled_tournaments = [x.strip() for x in f.readlines()]
 
     try:
         tournaments = crawl_tournaments()
         # TODO: Filter tournaments for Pauper (extend to other formats later)
         tournaments = set([x for x in tournaments if "pauper" in x.lower()])
         tournaments = sorted(tournaments, key=lambda x: int(x.split("-")[-1][:2]), reverse=True)
+        tournaments = [x for x in tournaments if x not in crawled_tournaments]
         pbar = tqdm(tournaments, desc="Crawling tournaments")
         for tournament in pbar:
             pbar.set_description(desc=f"Crawling {tournament}")
-            if tournament in crawled_tournaments:
-                print("Already crawled tournament:", tournament)
-                continue
             crawl_decks(tournament)
             time.sleep(1)
     except Exception as e:
