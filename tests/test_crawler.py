973a24f503e01ad356b9c1baeffe9f0585f46e25
chumpblocckami <mazzolauni@gmail.com>
2025-05-15 11:17:48 +0200

    feat: updated .gitignore

--- tests/test_crawler.py
+++
@@ -1,5 +0,0 @@
-from entrypoint import start_crawler
-
-if __name__ == "__main__":
-    # Add here some magic logic for debugging purposes
-    start_crawler()
--- tests/test_saver.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-
-from src.saver import Saver
-
-if __name__ == "__main__":
-    repo = Path(os.path.dirname(os.path.realpath(__file__))).parent
-    saver = Saver(str(repo))
-    saver.submit_changes(["./assets/pauper/9081_fszlaien.png"])
