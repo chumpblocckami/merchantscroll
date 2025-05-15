efed1c5dd9bddfdd55a3ab39876e60749d1d00a0
chumpblocckami <mazzolauni@gmail.com>
2025-05-15 08:54:20 +0200

    feat: updated tournaments

--- src/constants.py
+++ src/constants.py
@@ -7,7 +7,7 @@ HEADERS = {"User-Agent": "Mozilla/5.0"}
 PATTERN = r"window\.MTGO\.decklists\.data\s*=\s*({.*?});"
 
 DECKLISTS_FILE_PATH = str(Path("./assets/decklists.txt").resolve())
-TOURNAMENT_FILE_PATH = str(Path("./assets/tournaments.txt").resolve())
+TOURNAMENT_FILE_PATH = str(Path("./assets/pauper/tournaments.txt").resolve())
 
 REMOTE_REPO_URL = (
     f"https://x-access-token:{os.getenv('TOKEN_DECKS')}@github.com/chumpblocckami/mtg-decklists.git"
