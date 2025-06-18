cf8f81b8c330f1cfd3abf7db871f7653def84667
chumpblocckami <mazzolauni@gmail.com>
2025-06-18 11:43:34 +0200

    fix: removed modern format

--- src/constants.py
+++ src/constants.py
@@ -6,7 +6,7 @@ TIMEOUT = 60
 HEADERS = {"User-Agent": "Mozilla/5.0"}
 PATTERN = r"window\.MTGO\.decklists\.data\s*=\s*({.*?});"
 
-FORMATS = ["pauper", "modern"]  # ["pauper", "modern", "legacy"]
+FORMATS = ["pauper"]  # ["pauper", "modern", "legacy", "standard","vintage"]
 
 REMOTE_REPO_URL = (
     f"https://x-access-token:{os.getenv('TOKEN_DECKS')}@github.com/chumpblocckami/mtg-decklists.git"
