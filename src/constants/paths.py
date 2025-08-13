fc0fefcd1b326237d246b88ddca72fa811ef319a
chumpblocckami <mazzolauni@gmail.com>
2025-08-13 16:38:34 +0200

    fix: file format

--- src/constants/paths.py
+++ src/constants/paths.py
@@ -1,9 +1,14 @@
 from pathlib import Path
 
+from constants.misc import SAVE_FILE_AS
+
 TOURNAMENT_FILE_PATH = str(Path("./assets/pauper/tournaments.txt").resolve())
 RAW_TOURNAMENT_PATH = "./assets/{deck_format}/{tournament_id}.json"
 OUTPUT_PATH = "./{deck_format}/{reference_date}"
 OUTPUT_CONTENT_PATH = "./{deck_format}/{reference_date}/{deck_name}"
-REMOTE_PATH = "https://raw.githubusercontent.com/chumpblocckami/mtg-decklists/main/{deck_format}/{reference_date}/{deck_name}.png"  # noqa
+REMOTE_PATH = (
+    "https://raw.githubusercontent.com/chumpblocckami/mtg-decklists/main/{deck_format}/{reference_date}/{deck_name}."  # noqa
+    + SAVE_FILE_AS
+)
 REMOTE_DECKLISTS_PATH = "./assets/{deck_format}/decklists.txt"
 REMOTE_TOURNAMENTS_PATH = "./assets/{deck_format}/tournaments.txt"
